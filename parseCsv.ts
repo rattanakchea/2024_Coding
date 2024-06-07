810a1db23c9055450894c120ca0dc078c04b736f
Rattanak Chea <rattanak.chea@dpworld.com>
2024-06-06 23:37:39 -0700

    trade model

---
+++ parseCsv.ts
@@ -0,0 +1,37 @@
+import { CustomTrade, WebullTrade } from "./trade.model";
+
+// Include fs module
+const fs = require("fs");
+
+const fileName = "Webull_Orders_Records_Options.csv";
+
+// convert trades string to Array of string
+const trades = fs
+  .readFileSync(fileName, {
+    encoding: "utf-8",
+  })
+  .split("\n")
+  .map((row: string): string[] => {
+    return row.split(",");
+  });
+
+// Headers
+//   [
+//     'Name',          'Symbol',
+//     'Side',          'Status',
+//     'Filled',        'Total Qty',
+//     'Price',         'Avg Price',
+//     'Time-in-Force', 'Placed Time',
+//     'Filled Time\r'
+//   ],
+
+// Map to array of {name: value, side: value, ...}
+// console.log("trades: ", trades);
+
+for (let i = 1; i < trades.length; i++) {
+  const wTrade = new WebullTrade(trades[i]);
+  //   console.log("singleTrade", wTrade);
+  if (wTrade.status && wTrade.status.toLowerCase() !== "filled") continue;
+  const customTrade = new CustomTrade(wTrade);
+  console.log("customTrade", customTrade);
+}
---
+++ trade.model.ts
@@ -0,0 +1,49 @@
+// transformed Trade
+export interface ITrade {
+  name: string;
+  ticker: string;
+  entryDateTime: Date;
+  exitDateTime?: Date;
+}
+
+export class WebullTrade {
+  name: string; // SPY 06/11/2024 00:00:00 EDT Call $537.00
+  symbol: string;
+  side: "Buy" | "Sell" | string;
+  status: "Filled" | "Cancelled" | string;
+  filled: string;
+  totalQty: string;
+  price: string;
+  avgPrice: string;
+  timeInForce: string;
+  placedTime: string;
+  filledTime: string;
+
+  constructor(trades: string[]) {
+    // deconstruct trades array
+    // assign to Trade class members
+    [this.name, this.symbol, this.side, this.status, this.filled, this.totalQty, this.price, this.avgPrice, this.timeInForce, this.placedTime, this.filledTime] = trades;
+  }
+}
+
+export class CustomTrade {
+  name: string = "";
+  ticker: string = "";
+  expirationDate: string = "";
+  strike: string = "";
+  entryDateTime?: string = "";
+  exitDateTime?: string = "";
+
+  constructor(wTrade: WebullTrade) {
+    if (wTrade.status && wTrade.status.toLowerCase() !== "filled") {
+      console.error("The trade was not filled.");
+      return;
+    }
+
+    const [ticker, expirationDate, time, timezone, side, strike] = wTrade.name.split(" ");
+    this.name = wTrade.name;
+    this.ticker = ticker;
+    this.expirationDate = expirationDate;
+    this.strike = strike + side === "Call" ? "C" : "P";
+  }
+}
